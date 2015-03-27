218778765a6cf9a250081f04b544bf96ab8deaad
Manoj sk <skmanoj1988@gmail.com>
2015-03-27 11:14:42 +0530

    Adding nodemon locally and making necessary changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express')
-var app = express()
+    app = express()
+    port = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
-	res.send('hello')
+	res.send('hello nodemon')
 })
 
-app.listen(3000)
+app.listen(port, function() {
+	console.log("started the server at port: ",port)
+})
